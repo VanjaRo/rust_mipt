509d27a3afa8a054ae72d47f4760ba82319b065e
VanjaRo <vanjaro43@gmail.com>
2024-06-05 09:33:57 +0300

    feat: implemented bit_reader

--- problems/modules/ripgzip/src/bit_reader.rs
+++ problems/modules/ripgzip/src/bit_reader.rs
@@ -12,24 +12,30 @@ pub struct BitSequence {
 
 impl BitSequence {
     pub fn new(bits: u16, len: u8) -> Self {
-        // NB: make sure to zero unused bits so that Eq and Hash work as expected.
-        // TODO: your code goes here.
-        unimplemented!()
+        assert!(len <= 16, "the length shouldn't exceed 16 bits");
+
+        Self {
+            bits: bits,
+            len: len,
+        }
     }
 
     pub fn bits(&self) -> u16 {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.bits
     }
 
     pub fn len(&self) -> u8 {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.len
     }
 
     pub fn concat(self, other: Self) -> Self {
-        // TODO: your code goes here.
-        unimplemented!()
+        let total_len = self.len + other.len();
+        assert!(total_len <= 16);
+
+        Self {
+            bits: other.bits() << self.len() | self.bits(),
+            len: total_len,
+        }
     }
 }
 
@@ -37,25 +43,44 @@ impl BitSequence {
 
 pub struct BitReader<T> {
     stream: T,
-    // TODO: your code goes here.
+    reminder: BitSequence,
 }
 
 impl<T: BufRead> BitReader<T> {
     pub fn new(stream: T) -> Self {
-        // TODO: your code goes here.
-        unimplemented!()
+        Self {
+            stream,
+            reminder: BitSequence::new(0, 0),
+        }
     }
 
-    pub fn read_bits(&mut self, mut len: u8) -> io::Result<BitSequence> {
-        // TODO: your code goes here.
-        unimplemented!()
+    pub fn read_bits(&mut self, len: u8) -> io::Result<BitSequence> {
+        assert!(len <= 8);
+        if self.reminder.len() < len {
+            if let Some(fst_val) = self.stream.fill_buf()?.get(0) {
+                self.reminder = self.reminder.concat(BitSequence::new(*fst_val as u16, 8));
+                self.stream.consume(1);
+            } else {
+                return Err(io::Error::new(
+                    io::ErrorKind::UnexpectedEof,
+                    "can't read the requested amount of bits",
+                ));
+            }
+        }
+        let reminder_old = self.reminder;
+        // Getting values of the "len" amount of bits
+        let trailing_mask: u16 = (1 << len) - 1;
+        let bits_to_ret = reminder_old.bits() & trailing_mask;
+        self.reminder = BitSequence::new(reminder_old.bits() >> len, reminder_old.len() - len);
+
+        Ok(BitSequence::new(bits_to_ret, len))
     }
 
     /// Discard all the unread bits in the current byte and return a mutable reference
     /// to the underlying reader.
     pub fn borrow_reader_from_boundary(&mut self) -> &mut T {
-        // TODO: your code goes here.
-        unimplemented!()
+        self.reminder = BitSequence::new(0, 0);
+        &mut self.stream
     }
 }
 
@@ -69,7 +94,7 @@ mod tests {
     #[test]
     fn read_bits() -> io::Result<()> {
         let data: &[u8] = &[0b01100011, 0b11011011, 0b10101111];
-        let mut reader = BitReader::new(data);
+        let mut reader: BitReader<&[u8]> = BitReader::new(data);
         assert_eq!(reader.read_bits(1)?, BitSequence::new(0b1, 1));
         assert_eq!(reader.read_bits(2)?, BitSequence::new(0b01, 2));
         assert_eq!(reader.read_bits(3)?, BitSequence::new(0b100, 3));
