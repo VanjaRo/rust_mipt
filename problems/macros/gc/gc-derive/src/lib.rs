f0922ad10a5a1d8a4e6d941d7515b049126ab433
VanjaRo <vanjaro43@gmail.com>
2024-07-03 20:38:56 +0300

    feat: implemented proc_macro part of the gc

--- problems/macros/gc/gc-derive/src/lib.rs
+++ problems/macros/gc/gc-derive/src/lib.rs
@@ -1,9 +1,44 @@
 #![forbid(unsafe_code)]
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, FieldsNamed, Ident, Type};
+use syn::{parse_macro_input, Data, DeriveInput, Field, Fields, FieldsNamed};
 
 #[proc_macro_derive(Scan)]
 pub fn derive_scan(input: TokenStream) -> TokenStream {
-    // TODO: your code goes here.
+    // for a given struct collect all named fields
+    // and try to get references to them
+    let DeriveInput {
+        ident: ty_name,
+        generics,
+        data,
+        ..
+    } = parse_macro_input!(input);
+    let Data::Struct(strct) = data else {
+        panic!("underlying data should be struct")
+    };
+
+    let fields_quotes = match strct.fields {
+        Fields::Named(FieldsNamed { named: fields, .. }) => fields
+            .into_iter()
+            .map(|Field { ident: f_ident, .. }| {
+                quote!(
+                    ret_gcs.extend((&self.#f_ident as &dyn Scan).get_children_ref_adrrs());
+                )
+            })
+            .collect(),
+        _ => vec![],
+    };
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+    quote!(
+        impl #impl_generics Scan for #ty_name #ty_generics  #where_clause
+        {
+            fn get_children_ref_adrrs(&self) -> Vec<usize>{
+                let mut ret_gcs = Vec::new();
+                #(#fields_quotes)*
+                ret_gcs
+            }
+        }
+
+    )
+    .into()
 }
