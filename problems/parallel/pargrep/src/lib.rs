2069e327b6e2305a5914287ba86cb867c51506fd
VanjaRo <vanjaro43@gmail.com>
2024-07-23 21:38:35 +0300

    feat: implemented pargrep

--- problems/parallel/pargrep/src/lib.rs
+++ problems/parallel/pargrep/src/lib.rs
@@ -29,8 +29,42 @@ pub enum Event {
 }
 
 pub fn run<P: AsRef<Path>>(path: P, pattern: &str) -> Vec<Event> {
-    // TODO: your code goes here.
-    unimplemented!()
+    if path.as_ref().is_file() {
+        match File::open(path.as_ref()) {
+            Ok(file) => BufReader::new(file)
+                .lines()
+                .enumerate()
+                .filter(|(_, line)| line.is_err() || line.as_ref().unwrap().contains(pattern))
+                .map(|(line_idx, line)| match line {
+                    Ok(line_content) => Event::Match(Match {
+                        path: path.as_ref().to_path_buf(),
+                        line: line_content,
+                        line_number: line_idx + 1,
+                    }),
+                    Err(e) => Event::Error(Error {
+                        path: path.as_ref().to_path_buf(),
+                        error: e,
+                    }),
+                })
+                .collect(),
+            Err(e) => vec![Event::Error(Error {
+                path: path.as_ref().to_path_buf(),
+                error: e,
+            })],
+        }
+    } else {
+        match path.as_ref().read_dir() {
+            Ok(rd) => rd
+                .map(|entry| entry.unwrap().path())
+                .collect::<Vec<PathBuf>>()
+                .par_iter()
+                .map(|p| run(p, pattern))
+                .flatten()
+                .collect(),
+            Err(e) => vec![Event::Error(Error {
+                path: path.as_ref().to_path_buf(),
+                error: e,
+            })],
+        }
+    }
 }
-
-// TODO: your code goes here.
